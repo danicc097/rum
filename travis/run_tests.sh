8d16e1e68fdb8bc75877bf9e5f69dce2f9627edd
Pavel Borisov <pashkin.elfe@gmail.com>
2022-01-14 17:33:43 +0400

    modify travis testing conditions under HARDCORE

--- travis/run_tests.sh
+++ travis/run_tests.sh
@@ -62,7 +62,10 @@ pg_config
 if [ "$LEVEL" = "hardcore" ]; then
 
 	# perform static analyzis
-	scan-build --status-bugs make USE_PGXS=1 || status=$?
+	scan-build --status-bugs \
+	-disable-checker core.UndefinedBinaryOperatorResult \
+	-disable-checker deadcode.DeadStores \
+	make USE_PGXS=1 || status=$?
 
 	# something's wrong, exit now!
 	if [ $status -ne 0 ]; then exit 1; fi
